7f32dff720ac8a790cf02f385e445ad8137a62a6
IgorKrot <108532514+IgorKrot@users.noreply.github.com>
2022-12-20 11:33:00 +0200

    deploy server, add static setting

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const models = require("./models/models")
 const cors = require("cors")
 const router = require("./routes/index")
 const errorHandler = require("./middleware/ErrorHandlingMiddleware")
+const path = require('path');
+
 
 const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/api", router)
-
-
+app.use(express.static("../client/build"))
+app.get("*", (req, res) => {
+   res.sendFile(path.join("../client/build", "index.html"))
+})
 app.use(errorHandler)
 
 
